2f52ee933c0ba7de8a9bfdb082cd02fceb8cd7d3
François LaBerge <fgrclaberge@gmail.com>
2022-11-10 23:40:48 -0500

    add ica, pca, cca preprocessing to the randomforest classifier

--- src/models/randomforest.py
+++ src/models/randomforest.py
@@ -1,4 +1,4 @@
-from numpy import linspace, logspace
+from numpy import concatenate, linspace, logspace
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.utils.fixes import loguniform
@@ -6,6 +6,7 @@ from sklearn.utils.fixes import loguniform
 from src import config
 from src.models.abstractmodel import GaspipelineClassificationModel
 from src.preprocess.dataset import convert_binary_labels, remove_missing_values, scale_features
+from src.preprocess.featureselection import get_first_cca_feature, get_first_ica_feature, get_first_pca_feature
 
 
 class RandomForest(GaspipelineClassificationModel):
@@ -45,8 +46,19 @@ class RandomForest(GaspipelineClassificationModel):
     def _preprocess_features(self, x_train, x_test, y_train, y_test):
         x_train, y_train = remove_missing_values(x_train, y_train)
         x_test, y_test = remove_missing_values(x_test, y_test)
+
+        x_train_pca, pca = get_first_pca_feature(x_train)
+        x_train_cca, cca = get_first_cca_feature(x_train, y_train)
+        x_train_ica, ica = get_first_ica_feature(x_train)
+        x_test_pca = pca.transform(x_test)
+        x_test_cca = cca.transform(x_test)
+        x_test_ica = ica.transform(x_test)
+        x_train = concatenate((x_train_pca, x_train_cca, x_train_ica), axis=1)
+        x_test = concatenate((x_test_pca, x_test_cca, x_test_ica), axis=1)
+
         x_train, scaler = scale_features(x_train)
         x_test = scaler.transform(x_test)
+
         y_train = convert_binary_labels(y_train)
         y_test = convert_binary_labels(y_test)
         return x_train, x_test, y_train, y_test
