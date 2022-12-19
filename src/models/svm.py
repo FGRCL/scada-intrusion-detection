3a636ecda5e72f7e15eb84cb22b650392eff36af
François LaBerge <fgrclaberge@gmail.com>
2022-12-19 18:30:01 -0500

    add the preprocessing pipeline to SVMs

--- src/models/svm.py
+++ src/models/svm.py
@@ -1,14 +1,18 @@
 from os import cpu_count
 
+from imblearn.over_sampling import SMOTE
 from numpy import concatenate, linspace, logspace
 from scipy.stats import loguniform, norm, truncnorm
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.svm import SVC
 
 from src import config
 from src.models.abstractmodel import GaspipelineModelTrainer
+from src.preprocess.binarylabelencoder import BinaryLabelEncoder
 from src.preprocess.dataset import balance_dataset, convert_binary_labels, remove_missing_values, scale_features
+from src.preprocess.featureextraction import GasPipelineFeatureExtraction
 from src.preprocess.featureselection import get_first_cca_feature, get_first_ica_feature, get_first_pca_feature
 
 
@@ -48,85 +52,39 @@ class SvmTrainer(GaspipelineModelTrainer):
         return self.model
 
     def _preprocess_features(self, x_train, x_test, y_train, y_test):
-        x_train, y_train = remove_missing_values(x_train, y_train)
-        x_test, y_test = remove_missing_values(x_test, y_test)
+        simple_imputer = SimpleImputer()
+        binary_label_encoder = BinaryLabelEncoder()
 
-        y_train = convert_binary_labels(y_train)
-        y_test = convert_binary_labels(y_test)
+        x_train = simple_imputer.fit_transform(x_train, y_train)
+        x_test = simple_imputer.fit_transform(x_test, y_test)
+
+        y_train = binary_label_encoder.transform(y_train)
+        y_test = binary_label_encoder.transform(y_test)
         return x_train, x_test, y_train, y_test
 
 
 class GasPipelineSvc(BaseEstimator, ClassifierMixin):
     def __init__(self, balance_dataset=False, feature_reduction=False, scale_features=False, **kwargs):
-        self._scaler = None
-        self._ica = None
-        self._cca = None
-        self._pca = None
-        self.scale_features = scale_features
-        self.feature_reduction = feature_reduction
         self.balance_dataset = balance_dataset
+        self.feature_reduction = feature_reduction
+        self.scale_features = scale_features
+        self.feature_extraction = GasPipelineFeatureExtraction(self.feature_reduction, self.scale_features)
         self.svc = SVC(**kwargs)
 
     def fit(self, X, y):
-        X, y = self.preprocess_train(X, y)
+        if self.balance_dataset:
+            X, y = SMOTE().fit_resample(X, y)
+        X = self.feature_extraction.fit_transform(X, y)
         self.svc.fit(X, y)
 
-    def predict(self,X):
-        X, y = self.preprocess_test(X)
+    def predict(self, X):
+        X = self.feature_extraction.transform(X)
         return self.svc.predict(X)
 
     def score(self, X, y, sample_weight=None):
-        X, y = self.preprocess_test(X, y)
+        X = self.feature_extraction.transform(X)
         return self.svc.score(X, y, sample_weight)
 
     def set_params(self, **params):
-        self.scale_features = params.pop('scale_features', False)
-        self.feature_reduction = params.pop('feature_reduction', False)
-        self.balance_dataset = params.pop('balance_dataset', False)
-        self.svc.set_params(**params)
+        self.__init__(**params)
         return self
-
-    def preprocess_train(self, X, y):
-        if self.balance_dataset:
-            X, y = self.balance(X, y)
-
-        if self.feature_reduction:
-            X = self.reduction(X, y)
-
-        if self.scale_features:
-            X = self.scale(X)
-
-        return X, y
-
-    def preprocess_test(self, X, y=None):
-        if self.feature_reduction:
-            x_test_pca = self._pca.transform(X)
-            x_test_cca = self._cca.transform(X)
-            x_test_ica = self._ica.transform(X)
-            X = concatenate((x_test_pca, x_test_cca, x_test_ica), axis=1)
-
-        if self.scale_features:
-            X = self._scaler.transform(X)
-
-        return X, y
-
-    def balance(self, X, y):
-        return balance_dataset(X, y)
-
-    def reduction(self, X, y):
-        x_train_pca, pca = get_first_pca_feature(X)
-        x_train_cca, cca = get_first_cca_feature(X, y)
-        x_train_ica, ica = get_first_ica_feature(X)
-
-        self._pca = pca
-        self._cca = cca
-        self._ica = ica
-
-        X = concatenate((x_train_pca, x_train_cca, x_train_ica), axis=1)
-        return X
-
-    def scale(self, X):
-        X, scaler = scale_features(X)
-        self._scaler = scaler
-
-        return X
---
+++ src/preprocess/featureextraction.py
@@ -0,0 +1,58 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.cross_decomposition import CCA
+from sklearn.decomposition import FastICA, PCA
+from sklearn.pipeline import FeatureUnion, Pipeline
+from sklearn.preprocessing import StandardScaler
+from typing_extensions import override
+
+
+class GasPipelineFeatureExtraction(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_reduction=False, scale_features=False, n_components=1):
+        transforms = [
+            ('identity', IdentityTransformer())
+        ]
+
+        if feature_reduction:
+            transforms.append(
+                ('feature_reduction', FeatureUnion([
+                    ('pca', PCA(n_components=n_components)),
+                    ('cca', CCAWrapper(n_components=1)),
+                    ('ica', FastICA(n_components=n_components))
+                ]))
+            )
+
+        if scale_features:
+            transforms.append(
+                ('scaler', StandardScaler())
+            )
+
+        self.pipeline = Pipeline(steps=transforms)
+
+    def fit(self, X, y):
+        self.pipeline.fit(X, y)
+        return self
+
+    @override
+    def transform(self, X):
+        return self.pipeline.transform(X)
+
+
+class IdentityTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, input_array, y=None):
+        return self
+
+    @override
+    def transform(self, X):
+        return X
+
+
+class CCAWrapper(BaseEstimator, TransformerMixin):
+    def __init__(self, **kwargs):
+        self.cca = CCA(**kwargs)
+
+    def fit(self, X, y):
+        self.cca.fit(X, y)
+        return self
+
+    def transform(self, X):
+        return self.cca.transform(X)
